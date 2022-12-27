b23140bf1b56232a6787da5f0e030aa0708d56e3
Iuryglabson <iuryglabson18@gmail.com>
2022-12-27 09:39:56 -0300

    Feat: Implement Kafka publisher and consumer

--- Kafka-Producer/Program.cs
+++ Kafka-Producer/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+namespace kafkaConsumer
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "127.0.0.1:9092" };
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+                
+
+            {
+                try
+                {
+                    var count = 0;
+                    while (true)
+                    {
+                        var dr = await p.ProduceAsync("test-topic",
+                        new Message<Null, string> { Value = $"test {count++}" });
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}| {count}'");
+
+                        Thread.Sleep(2000);
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
